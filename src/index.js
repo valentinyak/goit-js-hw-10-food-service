5e49bc3a5da12c820036e91fb07f7dcbfaed1da4
valentinyak <63512456+valentinyak@users.noreply.github.com>
2020-10-06 22:41:37 +0300

    Добавляет код ДЗ 10

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import listTemplate from './template-menu-list.hbs';
+import menu from './menu.json';
+
+const menuListMurkup = listTemplate(menu);
+const refs = {
+  menuList: document.querySelector('ul.js-menu'),
+  checkbox: document.querySelector('#theme-switch-toggle'),
+};
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+checkCurrentTheme();
+refs.menuList.innerHTML = menuListMurkup;
+
+refs.checkbox.addEventListener('input', () => {
+  if (localStorage.getItem('userTheme') === Theme.DARK) {
+    setLightThemeOnBody();
+    return;
+  }
+  if (localStorage.getItem('userTheme') === Theme.LIGHT) {
+    setDarkThemeOnBody();
+  }
+});
+
+function checkCurrentTheme() {
+  if (localStorage.getItem('userTheme') === null) {
+    localStorage.setItem('userTheme', Theme.LIGHT);
+  }
+  if (localStorage.getItem('userTheme') === Theme.DARK) {
+    document.body.classList.add('dark-theme');
+    refs.checkbox.checked = true;
+  }
+}
+
+function setDarkThemeOnBody() {
+  document.body.classList.remove('light-theme');
+  document.body.classList.add('dark-theme');
+  localStorage.setItem('userTheme', Theme.DARK);
+}
+
+function setLightThemeOnBody() {
+  document.body.classList.remove('dark-theme');
+  document.body.classList.add('light-theme');
+  localStorage.setItem('userTheme', Theme.LIGHT);
+}
